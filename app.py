0e42c4259968a22494d26432fc6f65da68a19b51
Ojal <122396350+oj1o1@users.noreply.github.com>
2025-01-06 20:24:57 +0530

    app.py

--- app.py
+++ app.py
@@ -25,15 +25,7 @@ def input_pdf_text(uploaded_file):
 #Prompt Template
 
 input_prompt="""
-Hey Act Like a skilled or very experience ATS(Application Tracking System)
-with a deep understanding of tech field,software engineering,data science ,data analyst
-and big data engineer. Your task is to evaluate the resume based on the given job description.
-You must consider the job market is very competitive and you should provide 
-best assistance for improving thr resumes. Assign the percentage Matching based 
-on Jd and
-the missing keywords with high accuracy
-resume:{text}
-description:{jd}
+Hey act like you are HR of the company review resumes . also tell what skills are required tell the percentage of Job description match 
 
 I want the response in one single string having the structure
 {{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
